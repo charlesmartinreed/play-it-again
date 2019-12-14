d5b9ca7ce0183bfdcb04dc205917f4e5df744ba9
charlesmartinreed <charlesmartinreed@icloud.com>
2019-12-14 15:43:21 -0600

    Create active class to give UI feedback on played keys.

--- script.js
+++ script.js
@@ -9,6 +9,13 @@ function playNoteAudio(key) {
   let note = key.dataset.note;
   let noteAudio = document.getElementById(note);
 
+  key.classList.add("active");
+
+  //   this restarts the file at the beginning each time the function is triggered, allowing the user to rapidly fire the same note.
   noteAudio.currentTime = 0;
   noteAudio.play();
+
+  noteAudio.addEventListener("ended", () => {
+    key.classList.remove("active");
+  });
 }
