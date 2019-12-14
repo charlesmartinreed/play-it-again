c75e1671f0646a304c769034561d1ee13a84e7fc
charlesmartinreed <charlesmartinreed@icloud.com>
2019-12-14 15:59:30 -0600

    Add keydown event to play notes via keyboard.

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const keys = document.querySelectorAll(".piano-key");
 const keyAudio = document.querySelectorAll("audio");
 
+const WHITE_KEYS = ["z", "x", "c", "v", "b", "n", "m"];
+const BLACK_KEYS = ["s", "d", "g", "h", "j"];
+
+const whiteKeys = document.querySelectorAll(".piano-key-white");
+const blackKeys = document.querySelectorAll(".piano-key-black");
+
+// CLICK TO PLAY
 keys.forEach(key => {
   key.addEventListener("click", () => playNoteAudio(key));
 });
@@ -19,3 +26,14 @@ function playNoteAudio(key) {
     key.classList.remove("active");
   });
 }
+
+// PRESS TO PLAY
+document.addEventListener("keydown", e => {
+  const key = e.key;
+
+  const whiteKeyIndex = WHITE_KEYS.indexOf(key);
+  const blackKeyIndex = BLACK_KEYS.indexOf(key);
+
+  if (whiteKeyIndex > -1) playNoteAudio(whiteKeys[whiteKeyIndex]);
+  if (blackKeyIndex > -1) playNoteAudio(blackKeys[blackKeyIndex]);
+});
